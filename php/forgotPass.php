86db63b202fa32de6941f8b36d0e84cd8a89fc96
Stan Andrei <andrei.stan@info.uaic.ro>
2022-06-16 16:59:01 +0300

    Implemented missing account case

--- php/forgotPass.php
+++ php/forgotPass.php
@@ -12,14 +12,14 @@ if (!$con) {
 }
 
 
-$qry = "CREATE TABLE IF NOT EXISTS `users` (id int(255), password varchar(255), email varchar(255), password varchar(255))";
+$qry = "CREATE TABLE IF NOT EXISTS `users` (id int(255), username varchar(255), email varchar(255), password varchar(255))";
 $insert = mysqli_query($con, $qry);
 /*
 if (!$insert) {
 	echo "[Error] : Cannot create table <br>";
 	echo "<br>";
-} 
-else{ 
+}
+else{
 	echo "[Info] : Table created <br>";
 	echo "<br>";
 }
@@ -36,24 +36,33 @@ if (mysqli_num_rows($resultSet) > 0) {
   while($row = mysqli_fetch_assoc($resultSet)){
         $password = print_r($row['password'], True);
     }
+
+  $receiver = $email;
+  $subject = "Password recovery ColB";
+  $body = "Dear ColB user, \n\nThis is an automated message sent due to your account recovery request, your password is : '$password' \n\nThe ColB team wishes you a wonderful day.";
+  $sender = "From:colbfii@gmail.com";
+
+  if(mail($receiver, $subject, $body, $sender)) {
+      echo '<script type="text/javascript">';
+      echo 'alert("Your password has been sent via a email to your account");';
+      echo 'window.location.href = "../html/login.html";';
+      echo '</script>';
+  }
+  else {
+      echo '<script type="text/javascript">';
+      echo 'alert("There has been an issue and your password cannot be sent to your account");';
+      echo 'window.location.href = "../html/login.html";';
+      echo '</script>';
+  }
+
+}
+else {
+    echo '<script type="text/javascript">';
+    echo 'alert("There is no account tied to the introduced email");';
+    echo 'window.location.href = "../html/forgotUser.html";';
+    echo '</script>';
 }
 
-$receiver = $email;
-$subject = "Password recovery ColB";
-$body = "Dear ColB user, \n\nThis is an automated message sent due to your account recovery request, your password is : '$password' \n\nThe ColB team wishes you a wonderful day.";
-$sender = "From:colbfii@gmail.com";
-
-if(mail($receiver, $subject, $body, $sender)){
-    echo '<script type="text/javascript">';
-    echo 'alert("Your password has been sent via a email to your account");';
-    echo 'window.location.href = "../html/login.html";';
-    echo '</script>';
-}else{
-    echo '<script type="text/javascript">';
-    echo 'alert("There has been an issue and your password cannot be sent to your account");';
-    echo 'window.location.href = "../html/login.html";';
-    echo '</script>';
-}
 
 mysqli_close($con);
 ?>
--- php/forgotUser.php
+++ php/forgotUser.php
@@ -36,24 +36,33 @@ if (mysqli_num_rows($resultSet) > 0) {
   while($row = mysqli_fetch_assoc($resultSet)){
         $username = print_r($row['username'], True);
     }
+
+  $receiver = $email;
+  $subject = "Username recovery ColB";
+  $body = "Dear ColB user, \n\nThis is an automated message sent due to your account recovery request, your username is : '$username' \n\nThe ColB team wishes you a wonderful day.";
+  $sender = "From:colbfii@gmail.com";
+
+  if(mail($receiver, $subject, $body, $sender)) {
+      echo '<script type="text/javascript">';
+      echo 'alert("Your username has been sent via a email to your account");';
+      echo 'window.location.href = "../html/login.html";';
+      echo '</script>';
+  }
+  else {
+      echo '<script type="text/javascript">';
+      echo 'alert("There has been an issue and your username cannot be sent to your account");';
+      echo 'window.location.href = "../html/login.html";';
+      echo '</script>';
+  }
+
+}
+else {
+    echo '<script type="text/javascript">';
+    echo 'alert("There is no account tied to the introduced email");';
+    echo 'window.location.href = "../html/forgotUser.html";';
+    echo '</script>';
 }
 
-$receiver = $email;
-$subject = "Username recovery ColB";
-$body = "Dear ColB user, \n\nThis is an automated message sent due to your account recovery request, your username is : '$username' \n\nThe ColB team wishes you a wonderful day.";
-$sender = "From:colbfii@gmail.com";
-
-if(mail($receiver, $subject, $body, $sender)){
-    echo '<script type="text/javascript">';
-    echo 'alert("Your username has been sent via a email to your account");';
-    echo 'window.location.href = "../html/login.html";';
-    echo '</script>';
-}else{
-    echo '<script type="text/javascript">';
-    echo 'alert("There has been an issue and your username cannot be sent to your account");';
-    echo 'window.location.href = "../html/login.html";';
-    echo '</script>';
-}
 
 mysqli_close($con);
 ?>
